216ea309f6a811114f16a0a80c38549dafcdd040
unknown <bterzi@dsbsnciuniv.edu>
2023-12-09 23:28:30 +0300

    RATE CALISIYOOOOOOOOOOOOOOOOO

--- src/places/pages/Songs.js
+++ src/places/pages/Songs.js
@@ -1,23 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { AuthContext } from '../../shared/context/auth-context';
 
 import SongsList from '../components/SongsList';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { useHttpClient } from '../../shared/hooks/http-hook';
 
+
 const Songs = () => {
+  const auth = useContext(AuthContext); // Using AuthContext to access the token
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [loadedSongs, setLoadedSongs] = useState();
 
   useEffect(() => {
     const fetchSongs = async () => {
       try {
-        const responseData = await sendRequest('http://localhost:3000/songs');
+        const responseData = await sendRequest(
+          'http://localhost:3000/songs',
+          'GET',
+          null,
+          { Authorization: 'Bearer ' + auth.token }
+        );
+        console.log(responseData.songs); // Log to check the structure
         setLoadedSongs(responseData.songs);
-      } catch (err) {}
+      } catch (err) {
+        console.error(err);
+      }
     };
     fetchSongs();
-  }, [sendRequest]);
+  }, [sendRequest, auth.token]); // Include auth.token in the dependency array
 
   const songDeletedHandler = deletedSongId => {
     setLoadedSongs(prevSongs => prevSongs.filter(song => song._id !== deletedSongId));
@@ -35,7 +46,7 @@ const Songs = () => {
         <SongsList 
           items={loadedSongs} 
           onDeleteSong={songDeletedHandler} 
-          showAverageRating={true} // Show average rating in this context
+          showAverageRating={true}
         />
       )}
     </React.Fragment>
@@ -43,4 +54,3 @@ const Songs = () => {
 };
 
 export default Songs;
-
